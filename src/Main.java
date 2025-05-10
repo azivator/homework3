491989978a61cef7eeea3574a4b5bc3f55a4b25c
azivator <d.petrovskis99@gmail.com>
2025-05-11 02:25:58 +0300

    задача 3

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,23 @@ public class Main {
                 //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
                 // to see how IntelliJ IDEA suggests fixing it.
 
-                float a = 27.12f;
-                long b = 987678965549L;
-                float c = 2.786f;
-                short d = 569;
-                short e = -159;
-                int f = 27897;
-                byte g = 67;
-                System.out.println("Значение переменной 'a' с типом float равно " + a );
-                System.out.println("Значение переменной 'b' с типом long равно " + b );
-                System.out.println("Значение переменной 'c' с типом float равно " + c );
-                System.out.println("Значение переменной 'd' с типом short равно " + d );
-                System.out.println("Значение переменной 'e' с типом short равно " + e );
-                System.out.println("Значение переменной 'f' с типом int равно " + f );
-                System.out.println("Значение переменной 'g' с типом byte равно " + g );
+                byte LPStudents = 23;
+                byte ASStudents = 27;
+                byte EAStudents = 30;
+                short papers = 480;
+                System.out.println("У Людмилы Павловны " + LPStudents + " ученика");
+                System.out.println("У Анны Сергеевны " + ASStudents + " ученика");
+                System.out.println("У Екатерины Андреевны " + EAStudents + " ученика");
+                System.out.println("Всего куплено листов бумаги - " + papers + " шт.");
+                int paperPerStudent = papers / (LPStudents + ASStudents + EAStudents);
+                System.out.println("листов на ученика - " + paperPerStudent + " шт.");
+
+                //  float a = 27.12f;
+               // long b = 987678965549L;
+              //  float c = 2.786f;
+              //  short d = 569;
+              //  short e = -159;
+             //   int f = 27897;
+              //  byte g = 67;
         }
 }
