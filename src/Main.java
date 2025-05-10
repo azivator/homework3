f5d684c643ea86c8ba73a1aab29efca39830b8c1
azivator <d.petrovskis99@gmail.com>
2025-05-11 01:56:04 +0300

    задача 1

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+        public static void main(String[] args) {
+                //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+                // to see how IntelliJ IDEA suggests fixing it.
+                int a = 200;
+                byte b = 1;
+                short c = -1000;
+                long d = 1546345356;
+                float e = 12;
+                double f = 2.3456785431;
+                System.out.println("Значение переменной 'a' с типом byte равно " + a );
+                System.out.println("Значение переменной 'b' с типом byte равно " + b );
+                System.out.println("Значение переменной 'c' с типом byte равно " + c );
+                System.out.println("Значение переменной 'd' с типом byte равно " + d );
+                System.out.println("Значение переменной 'e' с типом byte равно " + e );
+                System.out.println("Значение переменной 'f' с типом byte равно " + f );
+        }
+}
