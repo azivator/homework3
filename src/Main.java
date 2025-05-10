9f3a4015693f873f313ddbac87a55fc0b676f03d
azivator <d.petrovskis99@gmail.com>
2025-05-11 02:51:43 +0300

    задача 5

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,18 @@ public class Main {
                 //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
                 // to see how IntelliJ IDEA suggests fixing it.
 
-                byte bottlePer2min = 16;
-                System.out.println("За 2 минуты машина произвела " + bottlePer2min + " штук бутылок");
-                int bottlePerMin = bottlePer2min / 2;
-                System.out.println("За минуту машина произвела " + bottlePerMin + " штук бутылок");
-                int bottlePer20Min = bottlePerMin * 20;
-                System.out.println("За 20 минут машина произвела " + bottlePer20Min + " штук бутылок");
-                int bottlePerDay = (bottlePerMin * 60) * 24;
-                System.out.println("За сутки машина произвела " + bottlePerDay + " штук бутылок");
-                int bottlePer3Day = bottlePerDay * 3;
-                System.out.println("За 3 дня машина произвела " + bottlePer3Day + " штук бутылок");
-                int bottlePerMonth = bottlePerDay * 31;
-                System.out.println("За месяц (31 день) машина произвела " + bottlePerMonth + " штук бутылок");
+                int z = 120;
+                System.out.println("всего купленно " + z + " банок краски двух цветов");
+                int x = 2;
+                System.out.println("на один класс нужно " + x + " банки белой краски");
+                int y = 4;
+                System.out.println("на один класс нужно " + y + " банки коричневой краски");
+                int a = 120/ (x+y);
+                System.out.println("в школе всего " + a + " классов");
+                int b = x*a;
+                System.out.println("на покраску всех классов было купленно " + b + " банок белой краски");
+                int c = y*a;
+                System.out.println("на покраску всех классов было купленно " + c + " банок коричневой краски");
+
         }
 }
