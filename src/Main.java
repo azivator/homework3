6ff8294fa472d7931b40d77fb4e3581ada976f6f
azivator <d.petrovskis99@gmail.com>
2025-05-11 11:59:17 +0300

    задача 6

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,17 @@ public class Main {
                 //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
                 // to see how IntelliJ IDEA suggests fixing it.
 
-                int z = 120;
-                System.out.println("всего купленно " + z + " банок краски двух цветов");
-                int x = 2;
-                System.out.println("на один класс нужно " + x + " банки белой краски");
-                int y = 4;
-                System.out.println("на один класс нужно " + y + " банки коричневой краски");
-                int a = 120/ (x+y);
-                System.out.println("в школе всего " + a + " классов");
-                int b = x*a;
-                System.out.println("на покраску всех классов было купленно " + b + " банок белой краски");
-                int c = y*a;
-                System.out.println("на покраску всех классов было купленно " + c + " банок коричневой краски");
-                System.out.println("В школе, где " +a+ " классов, нужно " +b+ " банок белой краски и " +c+ " банок коричневой краски");
-
+                int banana = 80;
+                System.out.println("Один банан весит " + banana + " грамм");
+                int milk = 105;
+                System.out.println("100 мл молока весит " + milk + " грамм");
+                int icecream = 100;
+                System.out.println("Один брикет пломбира весит " + icecream + " грамм");
+                int egg = 70;
+                System.out.println("Одно яйцо весит " + egg + " грамм");
+                int portion = 5*banana + 2*milk + 2*icecream + 4*egg;
+                System.out.println("Одна порция весит " + portion + " грамм");
+                float portionInKg = portion/1000f;
+                System.out.println("Одна порция весит " + portionInKg + " Кг");
         }
 }
