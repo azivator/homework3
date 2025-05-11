4adc93e8e6848114716732ee23a92f5b58bf6f55
azivator <d.petrovskis99@gmail.com>
2025-05-11 12:49:16 +0300

    задача 8

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,41 @@ public class Main {
                 //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
                 // to see how IntelliJ IDEA suggests fixing it.
 
-                int weightToLossKg = 7;
-                int weightToLoss = weightToLossKg*1000;
-                System.out.println("Нужно сбросить " + weightToLossKg + " килограмм, это "+weightToLoss+" грамм");
-                int loss250gPerDay = 250;
-                int daysIfLoss250PerDay = weightToLoss/loss250gPerDay;
-                System.out.println("Если терять по " + loss250gPerDay + " грамм в день, то понадобиться " + daysIfLoss250PerDay + " дней");
-                int loss500gPerDay = 500;
-                int daysIfLoss500PerDay = weightToLoss/loss500gPerDay;
-                System.out.println("Если терять по " + loss500gPerDay + " грамм в день, то понадобиться " + daysIfLoss500PerDay + " дней");
-                int AverageLossDays = (daysIfLoss250PerDay + daysIfLoss500PerDay)/2 ;
-                System.out.println("В среднем понадобиться " + AverageLossDays + " дней (Если считать среднее количество дней)");
-                float daysIfLossAverageWeigthPerDay = weightToLoss/((loss250gPerDay+loss500gPerDay)/2f)  ;
-                System.out.println("В среднем понадобиться " + daysIfLossAverageWeigthPerDay + " дней (Если считать через среднюю массу похудения в день по 375 грамм)");
+                int M = 67760;
+                int D = 83690;
+                int K = 76230;
+                float MAfterIncrease = M*1.1f;
+                float DAfterIncrease = D*1.1f;
+                float KAfterIncrease = K*1.1f;
+               // путь один
+                int MYearSalary =M*12;
+                int DYearSalary =D*12;
+                int KYearSalary =K*12;
+                float MIncreasedYearSalary =MAfterIncrease*12;
+                float DIncreasedYearSalary =DAfterIncrease*12;
+                float KIncreasedYearSalary =KAfterIncrease*12;
+                float MYearSalaryDiff =MIncreasedYearSalary-MYearSalary;
+                float DYearSalaryDiff =DIncreasedYearSalary-DYearSalary;
+                float KYearSalaryDiff =KIncreasedYearSalary-KYearSalary;
+                //путь два
+                float a =(MAfterIncrease-M)*12;
+                float b =(DAfterIncrease-D)*12;
+                float c =(KAfterIncrease-K)*12;
+
+                System.out.println("Маша получала " + M + " руб., после повышения зарплаты "+MAfterIncrease+" руб.");
+                System.out.println("Машин годовой доход вырос на " + MYearSalaryDiff + " руб.");
+                System.out.println("Денис получал " + D + " руб., после повышения зарплаты "+DAfterIncrease+" руб.");
+                System.out.println("Дениса годовой доход вырос на " + DYearSalaryDiff + " руб.");
+                System.out.println("Кристина получала " + K + " руб., после повышения зарплаты "+KAfterIncrease+" руб.");
+                System.out.println("Кристинин годовой доход вырос на " + KYearSalaryDiff + " руб.");
+                System.out.println("");
+                System.out.println("по второму пути решения полученые ответы:");
+                System.out.println("");
+                System.out.println("Маша получала " + M + " руб., после повышения зарплаты "+MAfterIncrease+" руб.");
+                System.out.println("Машин годовой доход вырос на " + a + " руб.");
+                System.out.println("Денис получал " + D + " руб., после повышения зарплаты "+DAfterIncrease+" руб.");
+                System.out.println("Дениса годовой доход вырос на " + b + " руб.");
+                System.out.println("Кристина получала " + K + " руб., после повышения зарплаты "+KAfterIncrease+" руб.");
+                System.out.println("Кристинин годовой доход вырос на " + c + " руб.");
         }
 }
